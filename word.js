ca45018ea8a974208405626ddbe03ebfe0a6b49f
not-damien <93546644+not-damien@users.noreply.github.com>
2022-12-27 10:12:18 -0800

    made stubs

--- word.js
+++ word.js
@@ -1,6 +1,10 @@
+words = ['hello', 'bye', 'java']
 console.log('helloworld')
 window.onload= function() {
     const tds = document.querySelectorAll("td")
+    words.forEach(word =>{
+        placeWord(word);
+    });
     tds.forEach(el => {
         if(el.textContent == ''){
             el.textContent = 'X'
@@ -8,4 +12,10 @@ window.onload= function() {
                 el.className = 'word'
             }
         });
+
+    }
+
+    function placeWord(theWord){
+        //todo stub
+        console.log("placing: " + theWord)
     }
