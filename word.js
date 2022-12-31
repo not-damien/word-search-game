22517ea4b59aa5be2b41280e6f508a66ac21a384
not-damien <93546644+not-damien@users.noreply.github.com>
2022-12-30 21:21:17 -0800

    Update word.js
    made method stubs and sudo code comments

--- word.js
+++ word.js
@@ -1,10 +1,25 @@
 words = ['hello', 'bye', 'java']
-console.log('helloworld')
+const rows = 15;
+const cols = 15;
+
+const gameboard = [];
+for (let i = 0; i < rows; i++) {
+  const row = [];
+  for (let j = 0; j < cols; j++) {
+    row.push("");
+  }
+  gameboard.push(row);
+} 
+
+
+
+
 window.onload= function() {
+    //placewords
+    //drawBoard
+    
+    
     const tds = document.querySelectorAll("td")
-    words.forEach(word =>{
-        placeWord(word);
-    });
     tds.forEach(el => {
         if(el.textContent == ''){
             el.textContent = 'X'
@@ -14,8 +29,25 @@ window.onload= function() {
         });
 
     }
-
-    function placeWord(theWord){
+    function placeWords(){
+        //iterate over list of words and select random starting locations/directions
+        //check if those locations are valid with canPlaceWord
+        //if can be placed call placeword else find new starting cordanates
+        
+        //after everything is placed fillremaining empty strings in 2d array with random letters
+    }
+    function placeWord(theWord,startingX, StartingY, direction){
         //todo stub
+        //place words into 2d array
         console.log("placing: " + theWord)
     }
+    function canPlaceWord(theWord, startingX, StartingY, direction){
+        //check if there are no conflicts
+    }
+    function drawBoard(){
+        //take characters from 2d array and put them in the html 
+        }
+
+
+//todo add way to take input from user and check their guesses
+
